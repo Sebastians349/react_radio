e92fd7cddef192147bf175b7e2685198a9afb7f9
Sebastian Simarro <ssebastian210@gmail.com>
2021-07-28 13:55:09 -0300

    Boton Top ok

--- src/components/Main/BackTopIcon.js
+++ src/components/Main/BackTopIcon.js
@@ -1,14 +1,45 @@
-import React from "react";
-import { Flex, Box, Button } from "@chakra-ui/react";
-import { BiArrowToTop } from "react-icons/bi";
+import React, { useState } from 'react';
+import { Tooltip, Button } from '@chakra-ui/react';
+import { FaArrowCircleUp } from 'react-icons/fa';
 
 const BackTopIcon = () => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  window.addEventListener('scroll', toggleVisible);
+
   return (
-    <Flex>
-      <Box pos="relative" zIndex={2} alignSelf="flex-end">
-        <Button as={BiArrowToTop} borderRadius="rounded" href="/index"></Button>
-      </Box>
-    </Flex>
+    <Button
+      pos="fixed"
+      w="7%"
+      h="6%"
+      left="90%"
+      bottom="80px"
+      z="1"
+      size="xxl"
+      cursor="pointer"
+      as={FaArrowCircleUp}
+      onClick={scrollToTop}
+      style={{ display: visible ? 'inline' : 'none' }}
+      borderRadius="full"
+      bg="transparent"
+      color="green.500"
+    ></Button>
   );
 };
 export default BackTopIcon;
